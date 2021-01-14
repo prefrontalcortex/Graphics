b681812fbcd889cb9b305eb9fe219462d77fbea9
Felix Herbst <herbst@prefrontalcortex.de>
2021-01-14 17:30:03 +0100

    added TextureSampler node that returns the SamplerState of a texture

---
+++ com.unity.shadergraph/Editor/Data/Nodes/Input/Texture/TextureSamplerNode.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using UnityEngine;
+using UnityEditor.Graphing;
+using UnityEditor.ShaderGraph.Drawing.Controls;
+using UnityEditor.ShaderGraph.Internal;
+
+namespace UnityEditor.ShaderGraph
+{
+    [Title("Input", "Texture", "Texture Sampler")]
+    class TextureSamplerNode : AbstractMaterialNode, IGeneratesBodyCode, IMayRequireMeshUV
+    {
+        public const int TextureInputId = 0;
+        public const int SamplerOutputId = 1;
+
+        const string kTextureInputName = "Texture";
+        const string kSamplerOutputName = "Sampler";
+
+        public override bool hasPreview { get { return false; } }
+
+        public TextureSamplerNode()
+        {
+            name = "Texture Sampler";
+            UpdateNodeAfterDeserialization();
+        }
+
+        public sealed override void UpdateNodeAfterDeserialization()
+        {
+            AddSlot(new Texture2DInputMaterialSlot(TextureInputId, kTextureInputName, kTextureInputName));
+            AddSlot(new SamplerStateMaterialSlot(SamplerOutputId, kSamplerOutputName, kSamplerOutputName, SlotType.Output));
+
+            RemoveSlotsNameNotMatching(new[] { TextureInputId, SamplerOutputId });
+        }
+
+        // Node generations
+        public virtual void GenerateNodeCode(ShaderStringBuilder sb, GenerationMode generationMode)
+        {
+            var id = GetSlotValue(TextureInputId, generationMode);
+            var result = string.Format("SAMPLER({0}) = {1};", GetVariableNameForSlot(SamplerOutputId), "sampler" + id);
+            sb.AppendLine(result);
+        }
+
+        public bool RequiresMeshUV(UVChannel channel, ShaderStageCapability stageCapability)
+        {
+            return true;
+        }
+    }
+}
